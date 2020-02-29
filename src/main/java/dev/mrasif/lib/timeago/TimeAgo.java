8508a1d5269956e37f45ed29a235589fc6700df8
Asif Mohammad Mollah <admin@mrasif.in>
2020-02-29 23:51:30 +0530

    chore: feature and test implemented

---
+++ src/main/java/dev/mrasif/lib/timeago/TimeAgo.java
@@ -0,0 +1,76 @@
+package dev.mrasif.lib.timeago;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TimeAgo {
+
+    /**
+     *
+     * @param past is a object of Date
+     * @return string data
+     */
+    public static final String format(Date past){
+        Date now=new Date();
+        return past!=null?format(past,now):null;
+    }
+
+    /**
+     *
+     * @param parseFormat string simpleDateFormat
+     * @param pastDate string timestamp
+     * @param currentDate string timestamp
+     * @return string result
+     * @throws ParseException date parse exception
+     */
+    public static final String format(String parseFormat, String pastDate, String currentDate) throws ParseException {
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(parseFormat);
+        Date past=simpleDateFormat.parse(pastDate);
+        Date now=simpleDateFormat.parse(currentDate);
+        return format(past,now);
+    }
+
+    /**
+     *
+     * @param past is a object of Date class
+     * @param now is a object of Date class
+     * @return string data
+     */
+    public static final String format(Date past, Date now) {
+        if (past==null || now==null){
+            return null;
+        }
+        long days= TimeUnit.MILLISECONDS.toDays(now.getTime() - past.getTime());
+        long hours=TimeUnit.MILLISECONDS.toHours(now.getTime() - past.getTime());
+        long minutes=TimeUnit.MILLISECONDS.toMinutes(now.getTime() - past.getTime());
+        long ms=TimeUnit.MILLISECONDS.toMillis(now.getTime() - past.getTime());
+        long seconds=ms/1000;
+        String result=null;
+        if (days>0 && days<30) {
+            result=(days>1?days+" days ago":"yesterday");
+        }
+        else if(days>=30 && days<365){
+            long months=(days/30);
+            result=(months>1?months+" months ago":"a month ago");
+        }
+        else if(days>=365){
+            long years=(days/365);
+            result=(years>1?years+" years ago":"a year ago");
+        }
+        else if(days==0 && hours>0){
+            result=(hours>1?hours+" hours ago":"an hour ago");
+        }
+        else if(hours==0 && minutes>0){
+            result=(minutes>1?minutes+" minutes ago":"a minute ago");
+        }
+        else if(minutes==0 && seconds>0){
+            result=(seconds>1?seconds+" seconds ago":"a second ago");
+        }
+        else{
+            result="a moment ago";
+        }
+        return result;
+    }
+}
---
+++ src/test/java/dev/mrasif/lib/timeago/TimeAgoTest.java
@@ -0,0 +1,156 @@
+package dev.mrasif.lib.timeago;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+public class TimeAgoTest {
+
+    SimpleDateFormat format;
+
+    @Before
+    public void setUp() throws Exception {
+        format = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+    }
+
+    @Test
+    public void format_with_null_value_parameters() {
+        String status=TimeAgo.format(null);
+        assertNull(status);
+    }
+
+    @Test
+    public void format_with_1_year_gap() throws Exception{
+        Date past = format.parse("28/01/2019 17:23:46");
+        Date now = format.parse("28/01/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("a year ago",is(status));
+    }
+
+    @Test
+    public void format_with_2_years_gap() throws Exception{
+        Date past = format.parse("28/01/2018 17:23:46");
+        Date now = format.parse("28/01/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("2 years ago",is(status));
+    }
+
+    @Test
+    public void format_with_1_month_gap() throws Exception{
+        Date past = format.parse("28/01/2020 17:23:46");
+        Date now = format.parse("28/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("a month ago",is(status));
+    }
+
+    @Test
+    public void format_with_2_months_gap() throws Exception{
+        Date past = format.parse("28/01/2020 17:23:46");
+        Date now = format.parse("28/03/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("2 months ago",is(status));
+    }
+
+    @Test
+    public void format_with_1_day_gap() throws Exception{
+        Date past = format.parse("28/02/2020 17:23:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("yesterday",is(status));
+    }
+
+    @Test
+    public void format_with_3_day_gap() throws Exception{
+        Date past = format.parse("26/02/2020 17:23:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("3 days ago",is(status));
+    }
+
+    @Test
+    public void format_with_1_hour_gap() throws Exception{
+        Date past = format.parse("29/02/2020 16:23:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("an hour ago",is(status));
+    }
+
+    @Test
+    public void format_with_2_hour_gap() throws Exception{
+        Date past = format.parse("29/02/2020 15:23:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("2 hours ago",is(status));
+    }
+
+    @Test
+    public void format_with_1_minute_gap() throws Exception{
+        Date past = format.parse("29/02/2020 17:22:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("a minute ago",is(status));
+    }
+
+    @Test
+    public void format_with_2_minutes_gap() throws Exception{
+        Date past = format.parse("29/02/2020 17:21:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("2 minutes ago",is(status));
+    }
+
+    @Test
+    public void format_with_1_second_gap() throws Exception{
+        Date past = format.parse("29/02/2020 17:23:45");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("a second ago",is(status));
+    }
+
+    @Test
+    public void format_with_2_seconds_gap() throws Exception{
+        Date past = format.parse("29/02/2020 17:23:44");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("2 seconds ago",is(status));
+    }
+
+    @Test
+    public void format_with_moment_gap() throws Exception{
+        Date past = format.parse("29/02/2020 17:23:46");
+        Date now = format.parse("29/02/2020 17:23:46");
+        String status=TimeAgo.format(past,now);
+        assertThat("a moment ago",is(status));
+    }
+
+    @Test
+    public void format_with_only_past_date_param() throws Exception{
+        Date past = format.parse("29/02/2019 17:23:46");
+        String status=TimeAgo.format(past);
+        assertNotNull(status);
+    }
+
+    @Test
+    public void format_date_parse_test() throws Exception{
+        String status=TimeAgo.format("dd/MM/yyyy hh:mm:ss","29/02/2020 17:23:44","29/02/2020 17:23:46");
+        assertThat("2 seconds ago",is(status));
+    }
+
+    @Test(expected = ParseException.class)
+    public void format_date_parse_test_with_invalid_format() throws Exception{
+        String status=TimeAgo.format("dd/MM/yyyy-hh:mm:ss","29/02/2020 17:23:44","29/02/2020 17:23:46");
+        assertThat("2 seconds ago",is(status));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        format=null;
+    }
+}
